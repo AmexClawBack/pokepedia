3a9583c6eb7632671c705cddc0d3cadb7ff56014
Dillon King <Dillonk2012@gmail.com>
2022-01-25 13:17:57 -0600

    Created Login component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-
-import './App.css';
+import { useContext } from "react";
+import { Routes, Route } from 'react-router-dom'
+import "./App.css";
 // Components
-import Nav from './components/Nav';
+import Nav from "./components/Nav";
+// pages
+import Login from "./pages/Login";
+// contexts
+import UserContext from "./contexts/UserContext";
 
 function App() {
+  // In able for us to use our context, we import first then we can use the useContext hook to access our context
+  // const user = useContext(UserContext)
+  // console.log(user)
+
   return (
     <div className="App">
-      
-      <Nav />
+      {/* All context comes with the Provider Component. This allows us to use this as a wrapper and share information to all of its children. We need the value prop inside our provider */}
+      <UserContext.Provider value={'christina'}>
+        <Nav />
+        {/* We need to wrap our all of our routes inside react router Routes component */}
+        <Routes>
+          <Route path='login' element={<Login />} />
+        </Routes>
+      </UserContext.Provider>
     </div>
   );
 }
---
+++ src/contexts/UserContext.js
@@ -0,0 +1,9 @@
+// import createContext
+import React, { createContext } from 'react'
+
+
+// Simplest way to create a context
+// SP: name your context the same as your file
+const UserContext = createContext(null)
+
+export default UserContext;
